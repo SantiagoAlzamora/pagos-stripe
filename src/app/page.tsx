93ebc0bb5134170baa4227e6a680bb572ac9b53f
SantiagoAlzamora <52084001+SantiagoAlzamora@users.noreply.github.com>
2024-02-21 16:41:21 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         'Content-Type': 'application/json'
       }
       
-      console.log(data.data)
+      console.info(data.data)
     }
     fetchProducts()
     
