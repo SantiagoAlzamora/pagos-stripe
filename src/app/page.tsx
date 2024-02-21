b25c4cddeec53e3c0b1ed12445df6d4eafff66c6
SantiagoAlzamora <52084001+SantiagoAlzamora@users.noreply.github.com>
2024-02-21 16:54:53 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,45 +9,45 @@ import axios from 'axios'
 import { IProduct } from "@/types/types"
 
 export default function Home() {
-  //const fetchProducts = useProductStore(state => state.fetchProducts)
-  const [products, setProducts] = useState<IProduct[]>([])
-  //const products = useProductStore((state) => state.products)
+  const fetchProducts = useProductStore(state => state.fetchProducts)
+  // const [products, setProducts] = useState<IProduct[]>([])
+  const products = useProductStore((state) => state.products)
   const selectedProduct = useProductStore(state => state.selectedProduct)
   useEffect(()=>{
-    const fetchProducts= async()=>{        
-        const productosResponse = await axios.get(`https://api.stripe.com/v1/products`, {
-        headers: {
-          'Authorization': `Bearer sk_test_51OC0qkGwtvH3PnHDyWTPHM12CfIkzI8j7Q5FFWjl1oZu6ACnNI89oCl3D1Lknacb3Q6qlCZvUlwvg53q95JyQnKT00HwAcUVkf`,
-          'Content-Type': 'application/json'
-        }
-      });
+    // const fetchProducts= async()=>{        
+    //     const productosResponse = await axios.get(`https://api.stripe.com/v1/products`, {
+    //     headers: {
+    //       'Authorization': `Bearer sk_test_51OC0qkGwtvH3PnHDyWTPHM12CfIkzI8j7Q5FFWjl1oZu6ACnNI89oCl3D1Lknacb3Q6qlCZvUlwvg53q95JyQnKT00HwAcUVkf`,
+    //       'Content-Type': 'application/json'
+    //     }
+    //   });
       
       
-      const productos : IProduct[] = productosResponse.data.data;
+    //   const productos : IProduct[] = productosResponse.data.data;
       
-      // Obtener los precios de los productos de forma concurrente
-      const preciosPromises = productos.map((producto) => {
-        return axios.get(`https://api.stripe.com/v1/prices?product=${producto.id}`, {
-          headers: {
-            'Authorization': `Bearer sk_test_51OC0qkGwtvH3PnHDyWTPHM12CfIkzI8j7Q5FFWjl1oZu6ACnNI89oCl3D1Lknacb3Q6qlCZvUlwvg53q95JyQnKT00HwAcUVkf`,
-            'Content-Type': 'application/json'
-          }
-        });
-      });
+    //   // Obtener los precios de los productos de forma concurrente
+    //   const preciosPromises = productos.map((producto) => {
+    //     return axios.get(`https://api.stripe.com/v1/prices?product=${producto.id}`, {
+    //       headers: {
+    //         'Authorization': `Bearer sk_test_51OC0qkGwtvH3PnHDyWTPHM12CfIkzI8j7Q5FFWjl1oZu6ACnNI89oCl3D1Lknacb3Q6qlCZvUlwvg53q95JyQnKT00HwAcUVkf`,
+    //         'Content-Type': 'application/json'
+    //       }
+    //     });
+    //   });
   
-      const preciosResponses : any[] = await Promise.all(preciosPromises);
+    //   const preciosResponses : any[] = await Promise.all(preciosPromises);
       
-      // Procesar los resultados de las solicitudes de precios
-      const productosConPrecio : IProduct[] = [];
-      for (let i = 0; i < preciosResponses.length; i++) {
+    //   // Procesar los resultados de las solicitudes de precios
+    //   const productosConPrecio : IProduct[] = [];
+    //   for (let i = 0; i < preciosResponses.length; i++) {
         
-        const precioResponse = preciosResponses[i];
-        const precio = precioResponse.data.data;
+    //     const precioResponse = preciosResponses[i];
+    //     const precio = precioResponse.data.data;
         
-        productosConPrecio.push({...productos[i], price: precio[0].unit_amount/100 });
-      }
-      setProducts(productosConPrecio)
-    }
+    //     productosConPrecio.push({...productos[i], price: precio[0].unit_amount/100 });
+    //   }
+    //   setProducts(productosConPrecio)
+    // }
     fetchProducts()
     
   },[])
